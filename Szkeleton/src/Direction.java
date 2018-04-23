bb388fbbd28ade48561aa61cd2573ed42e2e8d54
CptKukiFasz <38591918+CptKukiFasz@users.noreply.github.com>
2018-04-23 02:21:54 +0200

    Update Direction.java

--- Szkeleton/src/Direction.java
+++ Szkeleton/src/Direction.java
@@ -1,3 +1,5 @@
+//Ez az enumeráció egy mező (Field) lehetséges szomszédos irányait reprezentálja:
+//fel (up), le (down), balra (left) vagy jobbra (right).
 
 public enum Direction {
 	up, down, left, right
