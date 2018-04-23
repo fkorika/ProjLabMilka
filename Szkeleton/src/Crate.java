fe1883fac0b36e789e1f0d274bd5392c86d21741
CptKukiFasz <38591918+CptKukiFasz@users.noreply.github.com>
2018-04-23 02:21:23 +0200

    Update Crate.java

--- Szkeleton/src/Crate.java
+++ Szkeleton/src/Crate.java
@@ -1,6 +1,10 @@
 import java.io.FileWriter;
 import java.io.IOException;
 
+//Egy ládát reprezentál. Egy láda, ha lyukba esik, akkor képes meghalni.
+//Ha éppen megtolják, akkor képes arra, hogy másik ládát toljon, illetve,
+//ha egy munkást rátolnak, képes arra, hogy megölje az munkást.
+
 public class Crate extends Movable {
 
 	Warehouse warehouse;
@@ -11,11 +15,12 @@ public class Crate extends Movable {
 		warehouse = w;
 		warehouse.addCrate(this);
 	}
-
+	//Meghívja a Warehouse deleteCrate(c: Crate) függvényét önmagával,
+	//illetve a Field-je Movable attribútumát null-ra állítja.
 	public void die() {
 		warehouse.deleteCrate(this);
 	}
-
+	
 	public void setField(Field f) {
 		field = f;
 	}
@@ -23,11 +28,11 @@ public class Crate extends Movable {
 	public Field getField() {
 		return field;
 	}
-
+	//Kiiíró függvény
 	public void printMovable(FileWriter output) throws IOException {
 		output.write('C');
 	}
-
+	//Megvizsgálja, hogy láda-e
 	public boolean isCrate() {
 		return true;
 	}
